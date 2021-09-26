b03e120d947a1d557ed6ecfdfd0f941b4bf6562d
PDLwork <pdlwork@163.com>
2021-09-26 18:10:05 +0800

    Update string.py

--- 1.0_Study/string.py
+++ 1.0_Study/string.py
@@ -1,3 +1,4 @@
+#如果可以修改的话那还是很牛逼的奥
 print("------------------------------字符串的驻留机制---------------------------")
 #三个变量名不同，但字符串相同，指向同一地址
 a = 'python'
@@ -7,10 +8,3 @@ c = '''python'''
 print(a, id(a))
 print(b, id(b))
 print(c, id(c))
-
-a = "hello$"
-b = "hello$"
-print(id(a))
-print(id(b))
-print(a == b)
-print(a is b)
