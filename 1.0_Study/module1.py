544f8224e12661dd53cc26128f07d8c061cfa631
pdlwork <pdlwork@163.com>
2021-10-20 20:47:13 +0800

    模块学习新增内容

--- 1.0_Study/module1.py
+++ 1.0_Study/module1.py
@@ -1,11 +1,17 @@
-import module2  #新增学习内容
-
-print(module2.fun1(4, 5, 6))
-module2.hi()
-
 import math
 
 print(math)
 print(id(math))
 print(type(math))
 print(math.pi)
+
+'''两种引用模块区别,尤其是函数使用时调用的区别'''
+'''引入模块其中一种功能'''
+from module2 import fun1
+
+print(fun1(6, 5, 6))
+
+'''引入模块的全部功能'''
+import module2
+print(module2.fun1(4, 5, 6))
+module2.hi()
--- 1.0_Study/module2.py
+++ 1.0_Study/module2.py
@@ -3,3 +3,8 @@ def fun1(a, b, c):
 
 def hi():
     print('hi')
+
+
+#表示只有这个文件作为主程序运行的时候才执行以下语句
+if __name__ == '__main__':
+    print('test')
