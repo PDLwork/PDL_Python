91af238877ed6b3235ee85146d31fd07dbbb7748
pdlwork <pdlwork@163.com>
2021-10-22 22:00:32 +0800

    加入了for循环   可以使用

--- TxttoExcel/test.py
+++ TxttoExcel/test.py
@@ -1,30 +1,25 @@
-# import os
-# from typing import Awaitable, Text
 import xlwt
 
-# a = os.getcwd()     #获取当前目录
-# print (a)       #打印当前目录
-# os.chdir('E:/Project/Python/PDL_Python/TxttoExcel')     #定位到新的目录，请根据你自己文件的位置做相应的修改
-# a = os.getcwd()     #获取定位之后的目录
-# print(a)        #打印定位之后的目录
-
 txt1 = open('E:/Project/Python/PDL_Python/TxttoExcel/test.txt')
-a = txt1.read()     #原字符串
-print(a)
+string1 = txt1.read()     #原字符串
+print(string1)
 print()
 
-list1 = a.split()   #第一层以回车分割
+list1 = string1.split()   #第一层以回车分割
 print(list1)
+len1 = len(list1)
 print(list1[0])
 print()
 
 list2 = list1[0].split(',')     #第二层以逗号分割
 print(list2)
+len2 = len(list2)
 print(list2[0])
 print()
 
 list3 = list2[0].split(':')     #第三层以冒号分割
 print(list3)
+print()
 
 #创建一个workbook对象，相当于创建一个Excel文件
 book = xlwt.Workbook(encoding='utf-8',style_compression=0)
@@ -35,10 +30,25 @@ sheet = book.add_sheet('date', cell_overwrite_ok=True)
 # 其中的date是这张表的名字,cell_overwrite_ok，表示是否可以覆盖单元格，其实是Worksheet实例化的一个参数，默认值是False
 
 # 向表中添加表题
-sheet.write(0, 0, 'X')  # 其中的'0-行, 0-列'指定表中的单元，'X'是向该单元写入的内容
-sheet.write(0, 1, 'Y')
-sheet.write(0, 2, 'Z')
+sheet.write(0, 0, 'Test')  # 其中的'0-行, 0-列'指定表中的单元，'X'是向该单元写入的内容
+sheet.write(0, 1, 'World')
+sheet.write(0, 2, 'Hello')
 
-sheet.write(1, 0, list3[1])
+x = 1
+y = 0
+
+for first in list1:
+    print(first)
+    list2 = first.split(',')
+    for second in list2:
+        print(second)
+        list3 = second.split(':')
+        for third in list3:
+            print(third)
+            sheet.write(x, y, list3[1])
+        y += 1
+        if y > 2:
+            y = 0
+    x += 1
 
 book.save(r'E:/Project/Python/PDL_Python/TxttoExcel/test.xls')
