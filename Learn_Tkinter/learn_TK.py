e9cd9ac85f1a8fe3ad0b4d36391b374e71133ff7
PDLwork <pdlwork@163.com>
2021-10-28 13:39:34 +0800

    更新

--- Learn_Tkinter/learn_TK.py
+++ Learn_Tkinter/learn_TK.py
@@ -1,17 +1,12 @@
-import tkinter # 使用Tkinter前需要先导入
+import tkinter
 
-# 第1步，实例化object，建立窗口window
-window = tkinter.Tk()
-
-# 第2步，给窗口的可视化起名字
-window.title('My Window')
-
-# 第3步，设定窗口的大小(长 * 宽)
-window.geometry('500x300')  # 这里的乘是小x
+window = tkinter.Tk()       # 第1步，实例化object，建立窗口window
+window.title('My Window')       # 第2步，给窗口的可视化起名字
+window.geometry('500x300')  #第3步，设定窗口的大小(长 * 宽)这里的乘是小x
 
 # 第4步，在图形界面上设定标签
 l = tkinter.Label(window, text='你好！this is Tkinter', bg='green', font=('Arial', 12), width=30, height=2)
-# 说明： bg为背景，font为字体，width为长，height为高，这里的长和高是字符的长和高，比如height=2,就是标签有2个字符这么高
+# bg为背景，font为字体，width为长，height为高，这里的长和高是字符的长和高，比如height=2,就是标签有2个字符这么高
 
 # 第5步，放置标签
 l.pack()    # Label内容content区域放置位置，自动调节尺寸
