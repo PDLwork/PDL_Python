b0eb0d0147fc32a0faddef89c77669dbd6c81f50
PDLwork <pdlwork@163.com>
2021-10-28 13:44:09 +0800

    Update learn_TK.py

--- Learn_Tkinter/learn_TK.py
+++ Learn_Tkinter/learn_TK.py
@@ -4,7 +4,7 @@ window = tkinter.Tk()       # 第1步，实例化object，建立窗口window
 window.title('My Window')       # 第2步，给窗口的可视化起名字
 window.geometry('500x300')  #第3步，设定窗口的大小(长 * 宽)这里的乘是小x
 
-# 第4步，在图形界面上设定标签
+# 第4步，在 图形界面上设定标签
 l = tkinter.Label(window, text='你好！this is Tkinter', bg='green', font=('Arial', 12), width=30, height=2)
 # bg为背景，font为字体，width为长，height为高，这里的长和高是字符的长和高，比如height=2,就是标签有2个字符这么高
 
