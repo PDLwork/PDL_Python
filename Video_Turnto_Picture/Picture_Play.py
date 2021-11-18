8c5caef547eae155ff615b833d4a31984b010c54
PDLwork <pdlwork@163.com>
2021-11-18 20:39:38 +0800

    修改LGMD代码

--- Video_Turnto_Picture/Picture_Play.py
+++ Video_Turnto_Picture/Picture_Play.py
@@ -3,7 +3,7 @@ import os
 from itertools import cycle
 import cv2
 
-Picture_Path = 'D:/project/Python/PDL_Python/Video_Turnto_Picture/123'
+Picture_Path = 'E:/project/PDL_Python/Video_Turnto_Picture/123'
 
 # 列出frames文件夹下的所有图片
 filenames = os.listdir(Picture_Path)
@@ -24,16 +24,17 @@ while key & 0xFF != ord('p'):
 # import time
 # from itertools import cycle
 
-# Picture_Path = 'D:/project/Python/PDL_Python/Video_Turnto_Picture/123'
+# Picture_Path = 'E:/project/PDL_Python/Video_Turnto_Picture/123'
 
 # filenames = os.listdir(Picture_Path)
+# file_number = len(filenames)
 
-# img_iter = cycle([cv2.imread(os.sep.join([Picture_Path, x]), cv2.IMREAD_GRAYSCALE) for x in filenames])
+# img_iter = cycle([cv2.imread(os.sep.join([Picture_Path, 'hi%d.jpg' %x])) for x in range(file_number)])
 
 # fig = matplotlib.pyplot.figure(1)
 # matplotlib.pyplot.axis('off')       #关闭坐标轴
 
 # while True:
 #     matplotlib.pyplot.imshow(next(img_iter), cmap='gray')
-#     matplotlib.pyplot.draw()
-#     matplotlib.pyplot.pause(0.5)
+#     matplotlib.pyplot.show()
+#     matplotlib.pyplot.pause(0.042)
