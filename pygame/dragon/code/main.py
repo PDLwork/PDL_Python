35ebaccb86db31c965a6e6b1925f0acacabf36bb
PDLwork <pdlwork@163.com>
2022-01-28 21:44:49 +0800

    更新代码

--- pygame/dragon/code/main.py
+++ pygame/dragon/code/main.py
@@ -4,20 +4,28 @@ import sys
 pygame.init()  # 初始化pygame
 size = width, height = 734, 286  # 设置窗口大小
 screen = pygame.display.set_mode(size)  # 显示窗口
+clock = pygame.time.Clock()
 
 background = pygame.image.load('D:/project/Python/PDL_Python/pygame/dragon/picture/background1.png')  # 加载图片
 backgroundrect = background.get_rect()  # 获取矩形区域
 
-dragon = pygame.image.load('D:/project/Python/PDL_Python/pygame/dragon/picture/dragon1.png')
-dragonrect = dragon.get_rect()
-# dragonrect = dragonrect.move(50,210)
+dragon1 = pygame.image.load('D:/project/Python/PDL_Python/pygame/dragon/picture/dragon1.png')
+dragon2 = pygame.image.load('D:/project/Python/PDL_Python/pygame/dragon/picture/dragon2.png')
+dragonrect = dragon1.get_rect()
+dragonrect = dragonrect.move(50,210)
 
+flag = True
 while True:  # 死循环确保窗口一直显示
+    clock.tick(10)
     for event in pygame.event.get():  # 遍历所有事件
         if event.type == pygame.QUIT:  # 如果单击关闭窗口，则退出
             sys.exit()
 
     screen.blit(background, backgroundrect)  # 将图片画到窗口上
-    screen.blit(dragon, dragonrect)
+    if flag == True:
+        screen.blit(dragon1, dragonrect)
+    else:
+        screen.blit(dragon2, dragonrect)
+    flag = not flag
 
     pygame.display.flip()  # 更新全部显示
