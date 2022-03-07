da8c9cb717df0db322c3bcb2f7cbdddce0ad468c
PDLwork <pdlwork@163.com>
2022-03-08 00:43:45 +0800

    Update main.py

--- pygame/dragon/code/main.py
+++ pygame/dragon/code/main.py
@@ -1,16 +1,21 @@
 import pygame
 import sys
+import os
+
+#获取当前文件的目录，方便在别的电脑运行时读取相应文件
+Current_path = os.path.join(os.getcwd(), 'pygame', 'dragon', 'picture')
+Current_path = Current_path.replace('\\', '/')
 
 pygame.init()  # 初始化pygame
 size = width, height = 734, 286  # 设置窗口大小
 screen = pygame.display.set_mode(size)  # 显示窗口
 clock = pygame.time.Clock()
 
-background = pygame.image.load('D:/project/Python/PDL_Python/pygame/dragon/picture/background1.png')  # 加载图片
+background = pygame.image.load(Current_path + '/background1.png')  # 加载图片
 backgroundrect = background.get_rect()  # 获取矩形区域
 
-dragon1 = pygame.image.load('D:/project/Python/PDL_Python/pygame/dragon/picture/dragon1.png')
-dragon2 = pygame.image.load('D:/project/Python/PDL_Python/pygame/dragon/picture/dragon2.png')
+dragon1 = pygame.image.load(Current_path + '/dragon1.png')
+dragon2 = pygame.image.load(Current_path + '/dragon2.png')
 dragonrect = dragon1.get_rect()
 dragonrect = dragonrect.move(50,210)
 
