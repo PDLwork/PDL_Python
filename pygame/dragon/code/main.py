e97d6588cf5b4ca65ab3479e0d6edefe4b2745ab
PDLwork <pdlwork@163.com>
2022-03-08 01:10:21 +0800

    增加注释

--- pygame/dragon/code/main.py
+++ pygame/dragon/code/main.py
@@ -9,7 +9,7 @@ Current_path = Current_path.replace('\\', '/')
 pygame.init()  # 初始化pygame
 size = width, height = 734, 286  # 设置窗口大小
 screen = pygame.display.set_mode(size)  # 显示窗口
-clock = pygame.time.Clock()
+clock = pygame.time.Clock() #创建一个时间对象用于控制游戏运作的快慢
 
 background = pygame.image.load(Current_path + '/background1.png')  # 加载图片
 backgroundrect = background.get_rect()  # 获取矩形区域
@@ -17,16 +17,18 @@ backgroundrect = background.get_rect()  # 获取矩形区域
 dragon1 = pygame.image.load(Current_path + '/dragon1.png')
 dragon2 = pygame.image.load(Current_path + '/dragon2.png')
 dragonrect = dragon1.get_rect()
-dragonrect = dragonrect.move(50,210)
+dragonrect = dragonrect.move(50,210)    #将小恐龙移动到“地上”
 
-flag = True
+flag = True #创建一个flag标志用于在循环中判断使用哪张图片
 while True:  # 死循环确保窗口一直显示
     clock.tick(6)
     for event in pygame.event.get():  # 遍历所有事件
-        if event.type == pygame.QUIT:  # 如果单击关闭窗口，则退出
-            sys.exit()
+        if event.type == pygame.QUIT:  # 如果程序发现单击关闭窗口按钮
+            sys.exit()  #将窗口关闭
 
-    screen.blit(background, backgroundrect)  # 将图片画到窗口上
+    screen.blit(background, backgroundrect)  # 将背景图片画到窗口上
+
+    #根据flag标志确定显示的图片，这样可以造成小恐龙在跑的现象
     if flag == True:
         screen.blit(dragon1, dragonrect)
     else:
