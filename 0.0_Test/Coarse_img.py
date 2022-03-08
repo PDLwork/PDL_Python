6f587489a04ab93aad7f4cf4def5bb01a46aa854
PDLwork <pdlwork@163.com>
2022-03-08 12:41:58 +0800

    更新文件夹

--- 0.0_Test/Coarse_img.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy
-
-img = cv2.imread('D:/BaiduNetdiskDownload/ball.png', cv2.IMREAD_UNCHANGED)
-print(img.shape)
-
-cv2.imshow('test', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
