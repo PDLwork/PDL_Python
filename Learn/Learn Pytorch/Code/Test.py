c1ac4b8e1922496ae5d6d902704e40dccb698260
骚气小云哥 <90962463+PDLwork@users.noreply.github.com>
2022-03-12 21:06:04 +0800

    Update Test.py

--- Learn/Learn Pytorch/Code/Test.py
+++ Learn/Learn Pytorch/Code/Test.py
@@ -1,7 +1,9 @@
 from torch.utils.data import Dataset
 import cv2
+import os
 
 img_path = "F:/Project/PDL_Python/Learn/Learn Pytorch/Data/hymenoptera_data/hymenoptera_data/train/ants/0013035.jpg"
+img_path1 = "F:/Project/PDL_Python/Learn/Learn Pytorch/Data/hymenoptera_data/hymenoptera_data/train/ants"
 img = cv2.imread(img_path, cv2.IMREAD_COLOR)
 
 cv2.imshow('test', img)
@@ -11,6 +13,8 @@ cv2.imshow('test', img)
     
 #     def __getitem__(self, idx):
 
+print(os.listdir(img_path1))
+print(len(os.listdir(img_path1)))
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
