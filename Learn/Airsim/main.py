4359a735e4742e597a9bb16462f2240a4c496657
PDLwork <pdlwork@163.com>
2022-04-14 12:48:22 +0800

    更新代码

--- Learn/Airsim/main.py
+++ Learn/Airsim/main.py
@@ -1,41 +1,4 @@
 import airsim
-import numpy
-import cv2
-import matplotlib.pyplot
-import tkinter
-
-# 定义一个保存图片的函数，保存rgb和灰度图
-# 输入的是读取相机的数据和保存图片的索引
-def save_image(responses, prefix = ""):
-    response = responses[0]
-
-    # frombuffer将data以流的形式读入转化成ndarray对象 这一步只得到一个一维数组
-    buffer = numpy.frombuffer(response.image_data_uint8, dtype=numpy.uint8) 
-
-    # reshape把1维数组改为三维数组（得到三通道的RGB图像）
-    img_rgb = buffer.reshape(response.height, response.width, -1)
-    # 得到灰度图
-    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
-
-    # 保存图片
-    cv2.imwrite('img/RGB/'+str(prefix)+'.png', img_rgb)
-    cv2.imwrite('img/Grayscale/'+str(prefix)+'.png', img_gray)
-
-    # plt展示图片
-    matplotlib.pyplot.subplot(121)
-    matplotlib.pyplot.title('RGB image')
-    matplotlib.pyplot.axis('off')
-    img_rgb = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2BGR)
-    matplotlib.pyplot.imshow(img_rgb)
-
-    matplotlib.pyplot.subplot(122)
-    matplotlib.pyplot.title('Grayscale image')
-    matplotlib.pyplot.axis('off')
-    matplotlib.pyplot.imshow(img_gray, cmap='gray')
-
-    matplotlib.pyplot.show(block = False)      # 垃圾函数  设计得一点都不好
-    # matplotlib.pyplot.ion()     # 可交互
-    matplotlib.pyplot.pause(0.01)   #给他反应时间？
 
 if __name__ == "__main__":
     client = airsim.MultirotorClient()  # 与airsim创建链接
